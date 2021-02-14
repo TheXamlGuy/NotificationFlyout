b920a4622af6635cecb4fa33b92c39f102eab937
Daniel Clark <dan@thexamlguy.com>
2021-02-14 22:03:12 +0000

    Support setting notification tray icon as a png

--- src/TheXamlGuy.NotificationFlyout.Shared.UI/Extensions/ImageSourceExtensions.cs
+++ src/TheXamlGuy.NotificationFlyout.Shared.UI/Extensions/ImageSourceExtensions.cs
@@ -18,14 +18,23 @@ namespace TheXamlGuy.NotificationFlyout.Shared.UI.Extensions
                 var uri = $"{AppDomain.CurrentDomain.BaseDirectory}{bitmapImage.UriSour‌​ce}".Replace("ms-appx:///", "").Replace("/", "\\");
 
                 using var stream = File.OpenRead(uri);
-                return new Icon(stream, new Size(PInvoke.GetSystemMetricsForDpi((int)SystemMetricFlag.SM_CXICON, dpi), PInvoke.GetSystemMetricsForDpi((int)SystemMetricFlag.SM_CYICON, dpi)));
+                return ExtractIcon(dpi, stream);
             }
             else
             {
                 var storageFile = await Windows.Storage.StorageFile.GetFileFromApplicationUriAsync(bitmapImage.UriSource);
+
                 using var stream = await storageFile.OpenStreamForReadAsync();
-                return new Icon(stream, new Size(PInvoke.GetSystemMetricsForDpi((int)SystemMetricFlag.SM_CXICON, dpi), PInvoke.GetSystemMetricsForDpi((int)SystemMetricFlag.SM_CYICON, dpi)));
+                return ExtractIcon(dpi, stream);
             }
+        }
+
+        private static Icon ExtractIcon(uint dpi, Stream stream)
+        {
+            var bitmap = (Bitmap)Image.FromStream(stream);
+            var icon = Icon.FromHandle(bitmap.GetHicon());
+
+            return new Icon(icon, new Size(PInvoke.GetSystemMetricsForDpi((int)SystemMetricFlag.SM_CXICON, dpi), PInvoke.GetSystemMetricsForDpi((int)SystemMetricFlag.SM_CYICON, dpi)));
         }
 
         private enum SystemMetricFlag : int
