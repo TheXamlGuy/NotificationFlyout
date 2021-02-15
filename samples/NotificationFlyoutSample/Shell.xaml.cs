49169b1044509c86d49a1056a6819c5146856f4c
Daniel Clark <dan@thexamlguy.com>
2021-02-15 00:06:51 +0000

    [WIP] Non-functional now playing flyout sample

--- samples/NotificationFlyoutSample/Shell.xaml.cs
+++ samples/NotificationFlyoutSample/Shell.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace NotificationFlyoutSample
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Media.Control;
+
+namespace NotificationFlyoutSample
 {
     public sealed partial class Shell
     {
@@ -7,23 +11,6 @@
             InitializeComponent();
         }
 
-        private void Theme_SelectionChanged(object sender, Windows.UI.Xaml.Controls.SelectionChangedEventArgs e)
-        {
-            switch (Theme.SelectedIndex)
-            {
-                case 0:
-                    RequestedTheme = Windows.UI.Xaml.ElementTheme.Default;
-                    break;
-
-                case 1:
-                    RequestedTheme = Windows.UI.Xaml.ElementTheme.Dark;
-                    break;
-
-                case 2:
-                    RequestedTheme = Windows.UI.Xaml.ElementTheme.Light;
-                    break;
-            }
-        }
 
         private void MenuFlyoutItem_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
@@ -31,10 +18,17 @@
             app.Exit();
         }
 
-        private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        private async void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
-            var app = GetApplication();
-            app.OpenAsWindow<WindowContent>();
+            //var gsmtcsm = await GetSystemMediaTransportControlsSessionManager();
+            //var mediaProperties = await GetMediaProperties(gsmtcsm.GetCurrentSession());
+
         }
+
+        //private static async Task<GlobalSystemMediaTransportControlsSessionManager> GetSystemMediaTransportControlsSessionManager() =>
+        //await GlobalSystemMediaTransportControlsSessionManager.RequestAsync();
+
+        //private static async Task<GlobalSystemMediaTransportControlsSessionMediaProperties> GetMediaProperties(GlobalSystemMediaTransportControlsSession session) =>
+        //    await session.TryGetMediaPropertiesAsync();
     }
 }
